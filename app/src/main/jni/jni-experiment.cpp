aa93f447081fc605416cd21c37e9b99f28c63eb4
João Gonçalves <jsvgoncalves@redhat.com>
2016-02-01 19:38:16 +0100

    cpp: Add API class
    * Adds a class to simulate future API
    
    Signed-off-by: João Gonçalves <jsvgoncalves@redhat.com>

--- app/src/main/jni/jni-experiment.cpp
+++ app/src/main/jni/jni-experiment.cpp
@@ -7,14 +7,33 @@
 
 using namespace std;
 
+class API{
+    public:
+        API();
+        ~API();
+        string getString();
+    private:
+        string something;
+};
+
+API::API() {
+    something = "A random string!";
+}
+
+API::~API() {
+}
+
+string API::getString() {
+    return something;
+}
+
 extern "C" {
 
 jstring
 Java_com_example_jniexperiment_JniExperiment_helloString(JNIEnv *env,
                                                       jobject this_obj) {
-
-    string str = "Hello yet again, maybe?";
-    return env->NewStringUTF((const char *) str.c_str());
+    API api;
+    return env->NewStringUTF(api.getString().c_str());
 }
 
 }
