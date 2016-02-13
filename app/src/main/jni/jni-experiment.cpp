76cbf57ddc15ab80f1aa9f7669fa8546ad17f2c1
João Gonçalves <jsvgoncalves@redhat.com>
2016-02-13 23:25:13 +0100

    api: perform simple curl call
    * Performs a curl request to hardcoded link, needs
      to be refactored out (WIP #2)
    * Adds the necessary network permissions to Android
      Manifest file
    * Adds temporary logging to console from the C++ code
    
    Signed-off-by: João Gonçalves <jsvgoncalves@redhat.com>

--- app/src/main/jni/jni-experiment.cpp
+++ app/src/main/jni/jni-experiment.cpp
@@ -2,9 +2,9 @@
  * Red Hat OpenShift Mobile
  * 2016
  */
-#include <string>
 #include <jni.h>
 #include "openshift/OpenshiftAPI.h"
+// #include <android/log.h>
 
 
 using namespace std;
@@ -15,8 +15,9 @@ extern "C" {
 jstring
 Java_com_example_jniexperiment_JniExperiment_helloString(JNIEnv *env,
                                                       jobject this_obj) {
+    // __android_log_print(ANDROID_LOG_VERBOSE, "jniwebrest", "Starting app");
+
     OpenshiftAPI api;
-    //return env->NewStringUTF(api.getString().c_str());
     return env->NewStringUTF(api.useCurl().c_str());
 }
 
--- app/src/main/jni/openshift/OpenshiftAPI.cpp
+++ app/src/main/jni/openshift/OpenshiftAPI.cpp
@@ -4,6 +4,9 @@
  */
 
 #include "OpenshiftAPI.h"
+#include <sstream>
+#include <iostream>
+#include <android/log.h>
 
 OpenshiftAPI::OpenshiftAPI() {
     something = "A random, outsider, string!";
@@ -12,29 +15,68 @@ OpenshiftAPI::OpenshiftAPI() {
 OpenshiftAPI::~OpenshiftAPI() {
 }
 
-string OpenshiftAPI::getString() {
-    return something;
+
+/**
+ * A non-threadsafe simple libcURL-easy based HTTP downloader
+ */
+class HTTPDownloader {
+public:
+    HTTPDownloader();
+    ~HTTPDownloader();
+    /**
+     * Download a file using HTTP GET and store in in a std::string
+     * @param url The URL to download
+     * @return The download result
+     */
+    std::string download(const std::string& url);
+private:
+    void* curl;
+};
+
+
+size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream) {
+    string data((const char*) ptr, (size_t) size * nmemb);
+    *((stringstream*) stream) << data << endl;
+    return size * nmemb;
+}
+
+HTTPDownloader::HTTPDownloader() {
+    curl = curl_easy_init();
+}
+
+HTTPDownloader::~HTTPDownloader() {
+    curl_easy_cleanup(curl);
+}
+
+string HTTPDownloader::download(const std::string& url) {
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    // Follow redirection
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    // Prevent "longjmp causes uninitialized stack frame" bug
+    curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
+    curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING, "deflate");
+    std::stringstream out;
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &out);
+    // Perform the request, res will get the return code
+    CURLcode res = curl_easy_perform(curl);
+    // Check for errors
+    if (res != CURLE_OK) {
+        fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(res));
+        __android_log_print(ANDROID_LOG_VERBOSE, "jniwebrest", "Failed: %d", res);
+
+    }
+    __android_log_print(ANDROID_LOG_VERBOSE, "jniwebrest", "Result id: %d", res);
+
+    return out.str();
 }
 
 string OpenshiftAPI::useCurl() {
-    CURL *curl;
-    CURLcode res;
-
-    curl = curl_easy_init();
-    if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
-        /* example.com is redirected, so we tell libcurl to follow redirection */
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-
-        /* Perform the request, res will get the return code */
-        res = curl_easy_perform(curl);
-        /* Check for errors */
-        if(res != CURLE_OK)
-            fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                    curl_easy_strerror(res));
-
-        /* always cleanup */
-        curl_easy_cleanup(curl);
-    }
-    return "useCurl() return string";
+    HTTPDownloader downloader;
+    string content = downloader.download("https://openshift.redhat.com/broker/rest/cartridges");
+    cout << content << endl;
+    __android_log_print(ANDROID_LOG_VERBOSE, "jniwebrest", "cURL content: %s", content.c_str());
+    return content;
 }
